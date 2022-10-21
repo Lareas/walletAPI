f108753665db15a401e5972532f5305d804626af
LFA <lfernandoareas@gmail.com>
2022-10-20 22:50:49 -0300

    ended user repository

--- wallet3/src/main/java/com/wallet3/wallet3/repository/UserRepository.java
+++ wallet3/src/main/java/com/wallet3/wallet3/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.wallet3.wallet3.repository;
 import com.wallet3.wallet3.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailEquals(String email);
 }
--- wallet3/src/test/java/com/wallet3/wallet3/repository/UserRepositoryTest.java
+++ wallet3/src/test/java/com/wallet3/wallet3/repository/UserRepositoryTest.java
@@ -1,19 +1,42 @@
 package com.wallet3.wallet3.repository;
 
 import com.wallet3.wallet3.entity.User;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 //@ActiveProfiles("test")
 public class UserRepositoryTest {
 
+    private static final String EMAIL = "email@test.com";
+
     @Autowired
     UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        User u = new User();
+        u.setName("Setup User");
+        u.setPassword("Senha123456");
+        u.setEmail(EMAIL);
+        userRepository.save(u);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+
+
 
     @Test
     public void testSave() {
@@ -24,5 +47,14 @@ public class UserRepositoryTest {
 
         User response = userRepository.save(u);
         assertNotNull(response);
+    }
+
+    @Test
+    public void testFindByEmail() {
+        // usa o Optional porque pode existir um usuário ou não
+        Optional<User> response = userRepository.findByEmailEquals(EMAIL);
+
+        assertTrue(response.isPresent());
+        assertEquals(response.get().getEmail(), EMAIL);
     }
 }
